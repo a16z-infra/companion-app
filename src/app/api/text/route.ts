bf5e9ec04f2787c99f035ac288809223c5e80108
Yoko Li <ykhli1013@gmail.com>
2023-06-25 23:55:03 -0700

    Texting working

---
+++ src/app/api/text/route.ts
@@ -0,0 +1,63 @@
+import { NextResponse } from "next/server";
+import twilio from "twilio";
+import clerk from "@clerk/clerk-sdk-node";
+import dotenv from "dotenv";
+
+dotenv.config({ path: `.env.local` });
+const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+export async function POST(request: Request) {
+  let queryMap: any = {};
+  const twilioClient = twilio(accountSid, twilioAuthToken);
+  const data = decodeURIComponent(await request.text());
+  data.split("&").forEach((item) => {
+    queryMap[item.split("=")[0]] = item.split("=")[1];
+  });
+  const prompt = queryMap["Body"];
+  const serverUrl = request.url.split("/api/")[0];
+  const phoneNumber = queryMap["From"];
+
+  // check if the user has verified phone #
+  const users = await clerk.users.getUserList({ phoneNumber });
+  console.log("users: ", users);
+  if (!users || users.length == 0) {
+    return new NextResponse(
+      JSON.stringify({ Message: "User not authorized" }),
+      {
+        status: 401,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
+  const response = await fetch(`${serverUrl}/api/qa-pg-vector`, {
+    body: JSON.stringify({
+      prompt,
+      isText: true,
+      userId: users[0].id,
+      userName: users[0].firstName,
+    }),
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const responseText = await response.text();
+
+  const to = queryMap["From"];
+  const from = queryMap["To"];
+  await twilioClient.messages
+    .create({
+      body: responseText,
+      from,
+      to,
+    })
+    .catch((err) => {
+      console.log("WARNING: failed to send SMS.", err);
+    });
+
+  return NextResponse.json({ message: "Hello from the API!" });
+}
